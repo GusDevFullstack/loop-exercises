cdb0f1125b7e74175735d349950c483904be6a51
BareaDeveloper <bareadeveloper@gmail.com>
2026-02-11 19:21:07 -0300

    Cartesian System

---
+++ src/CartesianSystem.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class CartesianSystem {
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		while (x != 0 && y != 0) {
+			if (x > 0 && y > 0) {
+				System.out.println("First Quadrant");
+			} else if (x < 0 && y > 0) {
+				System.out.println("Second Quadrant");
+			} else if (x < 0 && y < 0) {
+				System.out.println("Third Quadrant");
+			} else {
+				System.out.println("Fourth Quadrant");
+			}
+			x = sc.nextInt();
+			y = sc.nextInt();
+		}
+		
+		sc.close();
+	}
+}
